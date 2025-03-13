53210bf894b54f8d5b305ee14a8996b8d02d0909
Oliver Blaser <58989552+oblaser@users.noreply.github.com>
2025-03-13 21:07:07 +0100

    added some GPIO test cases

--- readme.md
+++ readme.md
@@ -2,7 +2,7 @@
 
 > This repo uses submodules, so clone with `git clone --recurse-submodules https://github.com/oblaser/rpihal-system-test.git`
 
-This is (well, will be) the complete system test of [rpihal](https://github.com/oblaser/rpihal).
+This is the system test of [rpihal](https://github.com/oblaser/rpihal).
 It is also an example/template for projects using rpihal.
 
 
--- src/system-test/gpio.cpp
+++ src/system-test/gpio.cpp
@@ -23,6 +23,7 @@ using namespace system_test;
 
 static system_test::Case PullUp_PullDown();
 static system_test::Case Input();
+static system_test::Case Output();
 
 static void initGpioPin(system_test::TestObejct& to, int pin, const RPIHAL_GPIO_init_t* initStruct) noexcept(false);
 static void initGpioPins(system_test::TestObejct& to, uint64_t bits, const RPIHAL_GPIO_init_t* initStruct) noexcept(false);
@@ -45,6 +46,7 @@ system_test::Module system_test::GPIO()
 
     module.add(PullUp_PullDown());
     module.add(Input());
+    module.add(Output());
 
     resetGpioPins(module);
 
@@ -57,7 +59,7 @@ system_test::Case PullUp_PullDown()
 {
     Case tc(__func__);
 
-    constexpr uint64_t ledPinsBits = (RPIHAL_GPIO_BIT(GPIO_LED0) | RPIHAL_GPIO_BIT(GPIO_LED1));
+    constexpr uint64_t pinBits = (RPIHAL_GPIO_BIT(GPIO_LED0) | RPIHAL_GPIO_BIT(GPIO_LED1));
     RPIHAL_GPIO_init_t initStruct;
     RPIHAL_GPIO_defaultInitStruct(&initStruct);
     initStruct.mode = RPIHAL_GPIO_MODE_IN;
@@ -65,7 +67,7 @@ system_test::Case PullUp_PullDown()
     try
     {
         initStruct.pull = RPIHAL_GPIO_PULL_UP;
-        initGpioPins(tc, ledPinsBits, &initStruct);
+        initGpioPins(tc, pinBits, &initStruct);
 
         cli::check(tc, "is LED0 and LED1 on?");
     }
@@ -75,7 +77,7 @@ system_test::Case PullUp_PullDown()
     try
     {
         initStruct.pull = RPIHAL_GPIO_PULL_DOWN;
-        initGpioPins(tc, ledPinsBits, &initStruct);
+        initGpioPins(tc, pinBits, &initStruct);
 
         cli::check(tc, "is LED0 and LED1 off?");
     }
@@ -115,6 +117,7 @@ system_test::Case Input()
 {
     Case tc(__func__);
 
+    constexpr uint64_t pinBits = (RPIHAL_GPIO_BIT(GPIO_BTN0) | RPIHAL_GPIO_BIT(GPIO_BTN1));
     RPIHAL_GPIO_init_t initStruct;
     RPIHAL_GPIO_defaultInitStruct(&initStruct);
 
@@ -122,7 +125,7 @@ system_test::Case Input()
     initStruct.pull = RPIHAL_GPIO_PULL_NONE;
 
     // ignore result, already prints warnings and errors, and buttons are not directly connected
-    RPIHAL_GPIO_initPins((RPIHAL_GPIO_BIT(GPIO_BTN0) | RPIHAL_GPIO_BIT(GPIO_BTN1)), &initStruct);
+    RPIHAL_GPIO_initPins(pinBits, &initStruct);
 
     if (RPIHAL_GPIO_readPin(GPIO_BTN0) > 0) { cli::printWarning(tc, "BTN0 is pressed"); }
     if (RPIHAL_GPIO_readPin(GPIO_BTN1) > 0) { cli::printWarning(tc, "BTN1 is pressed"); }
@@ -132,18 +135,67 @@ system_test::Case Input()
     cli::instruct("press and hold BTN0");
     tc.assert(RPIHAL_GPIO_readPin(GPIO_BTN0) == 1);
     tc.assert(RPIHAL_GPIO_readPin(GPIO_BTN1) == 0);
+    tc.assert(((uint64_t)RPIHAL_GPIO_read() & pinBits) == RPIHAL_GPIO_BIT(GPIO_BTN0));
+    tc.assert((RPIHAL_GPIO_read64() & pinBits) == RPIHAL_GPIO_BIT(GPIO_BTN0));
+    // RPIHAL_GPIO_readHi() would need a compute module
 
     cli::instruct("press and hold BTN1");
     tc.assert(RPIHAL_GPIO_readPin(GPIO_BTN0) == 0);
     tc.assert(RPIHAL_GPIO_readPin(GPIO_BTN1) == 1);
+    tc.assert(((uint64_t)RPIHAL_GPIO_read() & pinBits) == RPIHAL_GPIO_BIT(GPIO_BTN1));
+    tc.assert((RPIHAL_GPIO_read64() & pinBits) == RPIHAL_GPIO_BIT(GPIO_BTN1));
+    // RPIHAL_GPIO_readHi() would need a compute module
 
     cli::instruct("press and hold BTN0 and BTN1");
     tc.assert(RPIHAL_GPIO_readPin(GPIO_BTN0) == 1);
     tc.assert(RPIHAL_GPIO_readPin(GPIO_BTN1) == 1);
+    tc.assert(((uint64_t)RPIHAL_GPIO_read() & pinBits) == pinBits);
+    tc.assert((RPIHAL_GPIO_read64() & pinBits) == pinBits);
+    // RPIHAL_GPIO_readHi() would need a compute module
 
     cli::instruct("release BTN0 and BTN1");
     tc.assert(RPIHAL_GPIO_readPin(GPIO_BTN0) == 0);
     tc.assert(RPIHAL_GPIO_readPin(GPIO_BTN1) == 0);
+    tc.assert(((uint64_t)RPIHAL_GPIO_read() & pinBits) == 0llu);
+    tc.assert((RPIHAL_GPIO_read64() & pinBits) == 0llu);
+    // RPIHAL_GPIO_readHi() would need a compute module
+
+    return tc;
+}
+
+system_test::Case Output()
+{
+    Case tc(__func__);
+
+    constexpr uint64_t pinBits = (RPIHAL_GPIO_BIT(GPIO_LED0) | RPIHAL_GPIO_BIT(GPIO_LED1));
+    RPIHAL_GPIO_init_t initStruct;
+    RPIHAL_GPIO_defaultInitStruct(&initStruct);
+    initStruct.mode = RPIHAL_GPIO_MODE_OUT;
+    initStruct.pull = RPIHAL_GPIO_PULL_NONE;
+
+    initGpioPins(tc, pinBits, &initStruct);
+
+
+
+    RPIHAL_GPIO_writePin(GPIO_LED0, 1);
+    RPIHAL_GPIO_writePin(GPIO_LED1, 1);
+    cli::check(tc, "is LED0 and LED1 on?");
+
+    RPIHAL_GPIO_writePin(GPIO_LED0, 0);
+    RPIHAL_GPIO_writePin(GPIO_LED1, 0);
+    cli::check(tc, "is LED0 and LED1 off?");
+
+    RPIHAL_GPIO_set(RPIHAL_GPIO_BIT(GPIO_LED0));
+    RPIHAL_GPIO_clr(RPIHAL_GPIO_BIT(GPIO_LED1));
+    cli::check(tc, "is LED0 on and LED1 off?");
+
+    RPIHAL_GPIO_clr(RPIHAL_GPIO_BIT(GPIO_LED0));
+    RPIHAL_GPIO_set(RPIHAL_GPIO_BIT(GPIO_LED1));
+    cli::check(tc, "is LED0 off and LED1 on?");
+
+    RPIHAL_GPIO_togglePin(GPIO_LED0);
+    RPIHAL_GPIO_togglePin(GPIO_LED1);
+    cli::check(tc, "is LED0 on and LED1 off?");
 
     return tc;
 }
